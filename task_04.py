8d83d79f14dbe076f617b459147d7795c8efa1e5
Iuliia Ambrokh <iuliia.ambrokh@spsmail.cuny.edu>
2016-10-27 23:43:43 +0000

    Task_04

--- task_04.py
+++ task_04.py
@@ -6,7 +6,8 @@ MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
 
-# You code goes here
+MYLEN = len(MYINPUT)
+LONGSTR = 'long' if MYLEN > MAX_LENGTH else LONGSTR
 
-OUTPUT = 'That certainly was a {} story!'.format(LONGSTR)
+OUTPUT = 'That certainly was a {0} story!'.format(LONGSTR)
 print OUTPUT
